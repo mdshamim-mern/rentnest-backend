66c71f49b54e50cf62d05d73ecf30bb477ef0d6c
Md Shamim <mdshamim.mern@gmail.com>
2026-07-09 02:32:39 +0600

    feat: added environment configuration setup

---
+++ src/config/index.ts
@@ -0,0 +1,20 @@
+import dotenv from 'dotenv';
+import path from 'path';
+
+dotenv.config({ path: path.join(process.cwd(), '.env') });
+
+export default {
+    port: process.env.PORT,
+    database_url: process.env.DATABASE_URL,
+    bcrypt_salt_rounds: process.env.BCRYPT_SALT_ROUNDS,
+    jwt: {
+        access_secret: process.env.JWT_ACCESS_SECRET,
+        refresh_secret: process.env.JWT_REFRESH_SECRET,
+        access_expires_in: process.env.JWT_ACCESS_EXPIRES_IN,
+        refresh_expires_in: process.env.JWT_REFRESH_EXPIRES_IN,
+    },
+    stripe: {
+        secret_key: process.env.STRIPE_SECRET_KEY,
+        webhook_secret: process.env.STRIPE_WEBHOOK_SECRET,
+    }
+};
