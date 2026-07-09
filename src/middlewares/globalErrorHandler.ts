26976986140812d4f1d55458a9cff8a0f3178283
mdshamim-mern <mdshamim.mern@gmail.com>
2026-07-09 09:29:04 +0600

    Update globalErrorHandler.ts

--- src/middlewares/globalErrorHandler.ts
+++ src/middlewares/globalErrorHandler.ts
@@ -6,11 +6,11 @@ const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
   let message = err.message || 'Something went wrong!';
   let errorDetails: any = err;
 
-  // ১. Zod Validation Error হ্যান্ডেল করা
+  
   if (err instanceof ZodError) {
     statusCode = 400;
     message = 'Validation Error';
-    // Zod এর বিশাল এরর অবজেক্ট থেকে শুধু দরকারি অংশটুকু বের করে নিচ্ছি
+  
     errorDetails = {
       issues: err.issues.map((issue) => ({
         field: issue.path[issue.path.length - 1],
@@ -18,7 +18,6 @@ const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
       })),
     };
   } 
-  // ২. Prisma Error হ্যান্ডেল করা (যেমন: একই ইমেইল দিয়ে দুইবার একাউন্ট খুলতে চাইলে)
   else if (err.name === 'PrismaClientKnownRequestError') {
     if (err.code === 'P2002') {
       statusCode = 400;
@@ -33,7 +32,6 @@ const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
     errorDetails = err.message;
   }
 
-  // অ্যাসাইনমেন্টের ম্যান্ডেটরি রিকোয়ারমেন্ট অনুযায়ী রেসপন্স পাঠানো হচ্ছে
   res.status(statusCode).json({
     success: false,
     message,
