33d16015052a7bd07b70bd613e1d29e5108225bc
mdshamim-mern <mdshamim.mern@gmail.com>
2026-07-09 09:29:43 +0600

    Update validateRequest.ts

--- src/middlewares/validateRequest.ts
+++ src/middlewares/validateRequest.ts
@@ -1,11 +1,9 @@
 import { NextFunction, Request, Response } from 'express';
-import { ZodType } from 'zod'; // ZodTypeAny এর বদলে সরাসরি ZodType নিয়ে আসলাম
+import { ZodType } from 'zod'; 
 import catchAsync from '../utils/catchAsync';
 
-// এখানে ZodType<any, any, any> ব্যবহার করেছি যাতে যেকোনো স্কিমা সে সাপোর্ট করে
 const validateRequest = (schema: ZodType<any, any, any>) => {
   return catchAsync(async (req: Request, res: Response, next: NextFunction) => {
-    // ইউজারের পাঠানো ডেটা (body, cookies) আমাদের Zod স্কিমার সাথে মিলিয়ে দেখছি
     await schema.parseAsync({
       body: req.body,
       query: req.query,
@@ -13,7 +11,6 @@ const validateRequest = (schema: ZodType<any, any, any>) => {
       cookies: req.cookies,
     });
     
-    // সব ঠিক থাকলে পরের ধাপে (কন্ট্রোলারে) পাঠিয়ে দিচ্ছি
     next();
   });
 };
