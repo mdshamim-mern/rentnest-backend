f3db3b196b247768161a04facb26a68abd27dc6c
mdshamim-mern <mdshamim.mern@gmail.com>
2026-07-09 09:37:40 +0600

    Update user.interface.ts

--- src/modules/user/user.interface.ts
+++ src/modules/user/user.interface.ts
@@ -1,18 +1,15 @@
-// ডাটাবেসে ইউজার সেভ করার টাইপ
 export interface IUser {
   name: string;
   email: string;
-  password?: string; // পাসওয়ার্ড রেসপন্সে পাঠাবো না, তাই অপশনাল
+  password?: string; 
   role: 'ADMIN' | 'LANDLORD' | 'TENANT';
 }
 
-// লগইন করার সময় যে বডি পাঠাবে তার টাইপ
 export interface ILoginPayload {
   email: string;
   password: string;
 }
 
-// সাইনআপ করার সময় যে বডি পাঠাবে তার টাইপ
 export interface ISignupPayload {
   name: string;
   email: string;
