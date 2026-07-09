72ec426aa7e9b4a6dae411ec6d74164050778dc3
mdshamim-mern <mdshamim.mern@gmail.com>
2026-07-09 09:32:12 +0600

    Update payment.route.ts

--- src/modules/payment/payment.route.ts
+++ src/modules/payment/payment.route.ts
@@ -5,10 +5,8 @@ import { Role } from '@prisma/client';
 
 const router = express.Router();
 
-// পেমেন্ট ইন্টেন্ট ক্রিয়েট করার রাউট
 router.post('/create-payment-intent', auth(Role.TENANT), PaymentController.createPaymentIntent);
 
-// পেমেন্ট ডাটাবেসে সেভ করার রাউট
 router.post('/', auth(Role.TENANT), PaymentController.savePayment);
 
 export const PaymentRoutes = router;
