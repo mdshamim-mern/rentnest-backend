6776ef209707ff33614b16cd8b5b2c0019ef9757
Md Shamim <mdshamim.mern@gmail.com>
2026-07-10 23:42:41 +0600

    Simplified Prisma client for Vercel build

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,11 +1,4 @@
 import { PrismaClient } from '@prisma/client';
-import { Pool } from 'pg';
-import { PrismaPg } from '@prisma/adapter-pg';
 
-const connectionString = process.env.DATABASE_URL;
+export const prisma = new PrismaClient();
 
-const pool = new Pool({ connectionString });
-
-const adapter = new PrismaPg(pool);
-
-export const prisma = new PrismaClient({ adapter });
